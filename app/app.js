8c9b5c44dc5f3e9cdb4f76047015793d384c3d7c
dotannetix <dotanmytech@gmail.com>
2016-08-30 21:23:53 +0300

    angular ng-click and ng-model and filter

--- app/app.js
+++ app/app.js
@@ -1,8 +1,35 @@
+angular.module("app", []).controller("ctrl1", ctrl1).controller("ctrl2", ctrl2);
+function ctrl1($scope,$http) {
+	$scope.name = "controller 1";
+	$scope.num = 4;
+	$scope.search = "walla";
+	$scope.showSearch = function() {
+		var newData = {	}
+		
+		newData.name=$scope.mySite;
+		newData.path = $scope.myPath;
+		$scope.sites.push(newData)
+	}
+	$scope.sites = [{
+		name : 'Walla',
+		path : "http://walla.co.il"
+	}, {
+		name : 'Sport5',
+		path : "http://sport5.co.il"
+	}, {
+		name : 'Ynet',
+		path : "http://www.ynet.co.il/"
 
- angular.module("app",[])
- .controller("ctrl1",ctrl1);
- 
- function ctrl1($scope){
- 	$scope.name="hello angular"
- 	$scope.colors=["red","black","blue"];
- }
+	}, {
+		name : 'angularjs',
+		path : "https://angularjs.org/"
+	}];
+
+}
+
+function ctrl2($scope) {
+
+	$scope.name = "controller 2"
+
+}
+
