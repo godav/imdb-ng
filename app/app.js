ed511eb2ccc31a1bf7affd439a3c4fa31875a120
avner ainouz <aainouz@univ.haifa.ac.il>
2016-10-06 17:18:33 +0300

    add the directive (fix to the right pllace)

--- app/app.js
+++ app/app.js
@@ -2,13 +2,17 @@
 (function(){
 	var imdb = angular.module("app", []);
 	
-	imdb.directive('fallbackSrc', function () {
-    var fallbackSrc = {
-    link: function postLink(scope, iElement, iAttrs) {
-      iElement.bind('error', function() {
-        angular.element(this).attr("src", iAttrs.fallbackSrc);
-      });
-    }
+	imdb.directive('movieDb', function () {
+    var movieDb = {
+    	restrict: 'A',
+    	templete: Name: {{}} lenght: {{}} actors: {{}} director: {{}}
+    	
+    	
+//    link: function postLink(scope, iElement, iAttrs) {
+//      iElement.bind('error', function() {
+//        angular.element(this).attr("src", iAttrs.fallbackSrc);
+//      });
+//    }
    }
    return fallbackSrc;
 });
